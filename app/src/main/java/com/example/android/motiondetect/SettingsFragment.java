47c5afa7dc5cb6a3341d7266ddaeb58a87870d7a
alexkandres <alexkandres@live.com>
2017-01-31 11:55:43 -0500

    Add Heading Category to settings

--- app/src/main/java/com/example/android/motiondetect/SettingsFragment.java
+++ app/src/main/java/com/example/android/motiondetect/SettingsFragment.java
@@ -1,14 +1,15 @@
 package com.example.android.motiondetect;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.app.DialogFragment;
 import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
     Preference datePicker;
+    Preference timePicker;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +17,11 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
 
         addPreferencesFromResource(R.xml.activity_seetings);
         datePicker = findPreference("datePicker");
+        timePicker = findPreference("timePicker");
 
         //refers to overriden onPreferenceClick below
         datePicker.setOnPreferenceClickListener(this);
+        timePicker.setOnPreferenceClickListener(this);
 
     }
 
@@ -30,12 +33,38 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     @Override
     public boolean onPreferenceClick(Preference preference) {
 
-        String key = preference.getKey();
-        if(key.equalsIgnoreCase("datePicker")){
-            Toast.makeText(getActivity(), "Date Picker", Toast.LENGTH_SHORT).show();
-            DialogFragment newDialogFragment = new DatePickerFragment();
-            newDialogFragment.show(getFragmentManager(), "DATE PICKER");
+/*      char grade = 'C';
+
+        switch(grade) {
+            case 'A' :
+                System.out.println("Excellent!");
+                break;
+            case 'B' :
+            case 'C' :
+                System.out.println("Well done");
+                break;
+            case 'D' :
+                System.out.println("You passed");
+            case 'F' :
+                System.out.println("Better try again");
+                break;
+            default :
+                System.out.println("Invalid grade");
         }
+        */
+        String key = preference.getKey();
+        switch(key){
+            case "datePicker":
+                DialogFragment newDialogFragment = new DatePickerFragment();
+                newDialogFragment.show(getFragmentManager(), "DATE PICKER");
+                break;
+            case "timePicker":
+                Toast.makeText(getActivity(), "Time Picker", Toast.LENGTH_SHORT).show();
+        }
+//        if(key.equalsIgnoreCase("datePicker")){
+//            DialogFragment newDialogFragment = new DatePickerFragment();
+//            newDialogFragment.show(getFragmentManager(), "DATE PICKER");
+//        }
         return false;
     }
 }
