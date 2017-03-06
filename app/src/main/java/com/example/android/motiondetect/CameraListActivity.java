e8f0f9154603a295fe6e54541bd18cb616c392f4
alexkandres <alexkandres@live.com>
2017-03-06 09:32:27 -0500

    Add url to recycler itemview

--- app/src/main/java/com/example/android/motiondetect/CameraListActivity.java
+++ app/src/main/java/com/example/android/motiondetect/CameraListActivity.java
@@ -7,11 +7,27 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.android.motiondetect.CameraListActivityFragment.cameraNameList;
+import static com.example.android.motiondetect.CameraListActivityFragment.urlList;
 
 public class CameraListActivity extends AppCompatActivity {
 
@@ -37,9 +53,21 @@ public class CameraListActivity extends AppCompatActivity {
 
                         //find Camera EditText view
                         //add text to list of cameras
-                        //Notigy adapter of change
+                        //Notify adapter of change
                         EditText editText = (EditText) ((AlertDialog)dialogInterface).findViewById(R.id.cameraNameId);
-                        CameraListActivityFragment.cameraNameList.add(editText.getText().toString());
+                        EditText editUrlText = (EditText) ((AlertDialog)dialogInterface).findViewById(R.id.urlId);
+
+                        //Add name and url to ArrayList
+                        String cameraName = editText.getText().toString();
+                        String urlName = editUrlText.getText().toString();
+
+                        cameraNameList.add(cameraName);
+                        urlList.add(urlName);
+
+                        //TODO send to postNewCamera
+                        postNewCamera(cameraName, urlName);
+
+                        //notify adapter that the data changed
                         CameraListActivityFragment.mAdapter.notifyDataSetChanged();
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -57,6 +85,48 @@ public class CameraListActivity extends AppCompatActivity {
         });
     }
 
+    public void postNewCamera(final String cameraName, final String urlName){
+
+        final String url = "http://ec2-54-242-89-175.compute-1.amazonaws.com:8000/api/camera/";
+        StringRequest strReq = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONArray reader;
+                try {
+                    reader = new JSONArray(response);
+                    Log.i("CameraListActivityyyy", "Post request works");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        },
+                new Response.ErrorListener(){
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+//                        hideDialog();
+                        Log.i("Error.Response", "Ahhhh");
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("Authorization", LoginActivity.token);
+                return map;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", cameraName);
+                params.put("address", urlName);
+                return params;
+            }
+        };
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
